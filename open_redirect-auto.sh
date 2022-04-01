00d997d8724e8d0a7300969ab3e88eafac10fc23
Rizwan <72160000+RizwanSiDdiqu1@users.noreply.github.com>
2022-04-01 06:59:50 -0800

    Update open_redirect-auto.sh

--- open_redirect-auto.sh
+++ open_redirect-auto.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
 waybackfile=$1
+#payload=/mnt/f/wordlist/open-redirect.txt
+count=0
 if [ -f $waybackfile ]
 then
-    for url in $(cat $waybackfile | grep -a -i \=http | qsreplace 'http://evil.com' )
+    for url in $(cat $waybackfile)
     do
-        title=$(curl -s -L $url | grep -i "<title>Evil.Com")
-        if [[ ! -z "$title" ]]
-        then
-            echo "$url is \033[0;31mVulnerable\n" >> open_Vul.txt
-        fi
+        echo "Url is picked"
+        for pay in $(cat /mnt/f/wordlist/open-redirect.txt)
+        do
+            echo "Payload is Picked"
+            final=$(echo $url |  grep -a -i \= | qsreplace $pay)
+            #echo $final
+            if [ ! -z "$final" ]
+            then
+                title=$(curl -s -L $final | grep -i "<title>Evil.Com")
+                if [[ ! -z "$title" ]]
+                then
+                    ((count=count+1))
+                    echo "$final is \033[0;31mVulnerable\n" | tee -a find-open-redirect.txt
+                    if [[ $count -eq 1 ]]
+                    then
+                        break
+                    fi
+                fi
+            fi
+        done
     done
 
 else
